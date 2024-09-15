28d6ee3f0e679f41041f8f89a1a7c350ae70696a
idrisRoti <idrisomisakin@gmail.com>
2024-09-15 06:09:41 +0100

    fix: remove chart component

--- components/backoffice/OverviewChart.tsx
+++ components/backoffice/OverviewChart.tsx
@@ -15,7 +15,7 @@ export default function OverviewChart<T>({data}:{data: T[]}) {
   return (
     <div className="w-full">
       <ResponsiveContainer>
-        <LineChart height={250} data={data}>
+        <LineChart width={500} height={250} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="key" />
           <YAxis />
